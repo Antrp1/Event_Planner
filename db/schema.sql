7642e57099c9dfec4f2942596ff86295edd1dd64
Jair Gomez <jairherber505@gmail.com>
2023-06-22 21:39:58 -0700

    added module category

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE products (
     id INT AUTO_INCREMENT,
     product_name VARCHAR(30) NOT NULL,
     price DECIMAL,
-    stock BIT,
+    stock BOOLEAN,
     category VARCHAR(30)
 )
 
--- models/product.js
+++ models/product.js
@@ -1,5 +1,51 @@
-// const Sequelize = require('sequelize');
-// const sequelize = require('../db'); //connection to database we'll use
+const { Model, DataTypes } = require("sequelize");
 
-// //future code
-// module.exports = Product;
+const sequelize = require("../config/connection.js");
+
+class Products extends Model {}
+
+Products.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    in_stock: {
+        type: DataTypes.VIRTUAL,
+        get: function () {
+            return this.stock>0
+        },
+        set(value) {
+      throw new Error('No in stock');
+    }
+    },
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "products",
+  }
+);
+
+module.exports = Product;
