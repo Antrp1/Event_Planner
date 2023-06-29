910590246a9bbccf032476f732d1a9b632cf3c10
Jair Gomez <jairherber505@gmail.com>
2023-06-28 17:35:46 -0700

    some small changes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,14 @@
+// Form function
 document.getElementById("showFormBtn").addEventListener("click", function () {
   document.getElementById("formContainer").style.display = "block";
 });
 
+// Button function
 document.getElementById("showFormBtn").addEventListener("click", function () {
   document.getElementById("showFormBtn").style.display = "none";
 });
 
+// Login function
+document.querySelector(".box").addEventListener("click", function () {
+  document.getElementById("loginContainer").style.display = "block";
+});
