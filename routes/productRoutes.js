71e9b5f2f316b92df80ff16eec5888ffb571daf7
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-06-28 21:12:25 -0700

    in routes folder: created and added code into categoryRoutes.js & index.js, added code into productRoutes.js. Typo error in category-seeds.js file: line 18. Modified index.js in models folder.

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,77 @@
-// needed modules connections
-const { application } = require('express');
-const express = require('express');
-const productController = require('../controllers/productController');
+const router = require('express').Router();
+const { Product } = require('../models');
 
+// Get all products
+router.get('/products', async (req, res) => {
+  try {
+    const productsData = await Product.findAll();
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get one product by id
+router.get('/products/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id);
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a new product
+router.post('/products', async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Update a product by id
+router.put('/products/:id', async (req, res) => {
+  try {
+    const productData = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData[0]) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete a product by id
+router.delete('/products/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
