d1730f23a1811cd76df59fd5dd16212e85bc9648
Jair Gomez <jairherber505@gmail.com>
2023-06-29 19:49:50 -0700

    small lil change

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-// const stripe = require("stripe")(
-//   "sk_test_51NOA4SDltZuFbXatKHv7ROPwdUQ13yVe8fsPhwFwjWsgWU1ElqQMIoLQMrG1tQm3ZDB9306j1BZbltPjKVTsfw7s006eHCJwKs"
-// );
 const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -18,8 +15,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "idk",
   cookie: {
-    // Stored in milliseconds
-    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    maxAge: 24 * 60 * 60 * 1000,
   },
   resave: false,
   saveUninitialized: true,
